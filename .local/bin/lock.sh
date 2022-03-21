6872f67ed332318aee9e2390f2b2c47181699127
Abdellah Lahnaoui <abdellah.lahnaoui@gmail.com>
2022-03-21 14:32:37 +0100

    Added indicator to lock screen

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -3,5 +3,12 @@
 scrot /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 [[ -f $1 ]] && convert /tmp/screen.png $1 -gravity center -composite -matte /tmp/screen.png
-i3lock -u -i /tmp/screen.png
+pkill -u $USER -USR1 dunst
+i3lock -n -i /tmp/screen.png \
+	--inside-color=373445ff --ring-color=ffffffff --line-uses-inside \
+	--keyhl-color=d23c3dff --bshl-color=d23c3dff --separator-color=00000000 \
+    	--insidever-color=fecf4dff --insidewrong-color=d23c3dff \
+	--ringver-color=ffffffff --ringwrong-color=ffffffff --ind-pos="x+86:y+1003" \
+    	--radius=15 --verif-text="" --wrong-text=""
 rm /tmp/screen.png
+pkill -u $USER -USR2 dunst
