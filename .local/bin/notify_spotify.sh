350c892e243c2c6fc1efb74dc1a4e89992d27312
Abdellah Lahnaoui <abdellah.lahnaoui@gmail.com>
2022-04-03 05:59:23 +0200

    Only send notification on song change

--- .local/bin/notify_spotify.sh
+++ .local/bin/notify_spotify.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-playerctl metadata mpris:artUrl | xargs wget -O /tmp/spotify_cover.jpg
-notify-send -i /tmp/spotify_cover.jpg -a "`playerctl metadata artist`" "`playerctl metadata title`" -t 3000
+
+# Filter events
+if [ $PLAYER_EVENT = 'change' ]
+then
+	playerctl metadata mpris:artUrl | xargs wget -O /tmp/spotify_cover.jpg
+	notify-send -i /tmp/spotify_cover.jpg \
+		-a "`playerctl metadata artist`" \
+		"`playerctl metadata title`" \
+		-t 3000
+fi
