46def7c5e037f5d3b616c34a706d8d4f4c30a0b3
luoabd <abdellah.lahnaoui@gmail.com>
2023-02-06 23:31:02 +0100

    Adjust spotify notifications

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -346,3 +346,18 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 
 -- Autostart apps
 awful.spawn.with_shell("~/.config/awesome/autorun.sh")
+
+-- Changing spotify notifications.
+naughty.config.presets.spotify = { 
+    -- if you want to disable Spotify notifications completely, return false
+    callback = function(args)
+        return true
+    end,
+
+    -- Adjust the size of the notification
+    height = 100,
+    width  = 400,
+    -- Guessing the value, find a way to fit it to the proper size later
+    icon_size = 90
+}
+table.insert(naughty.dbus.config.mapping, {{appname = "Spotify"}, naughty.config.presets.spotify})
